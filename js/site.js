b522fbde4c91a4b3bd45ba4067afda7ef3d1d1eb
Daniar Kainarbaev <lkjuftyt@gmail.com>
2026-04-12 00:10:59 +0300

    Add tournament label to head-to-head cards

--- js/site.js
+++ js/site.js
@@ -4147,9 +4147,13 @@ async function renderMatchPageFromJson() {
     const headToHeadMarkup = headToHeadMatches.length
       ? headToHeadMatches.map(match => {
           const scoreParts = String(match.score || '0:0').split(':');
+          const tournamentLabel = translateRuntimeText(normalizeTournamentDisplayName(match.tournament_name || 'Кубок Бурчалкина'));
           return `
             <a class="match-headtohead-row" href="${escapeHtml(getMatchPageUrl(match))}">
-              <div class="match-headtohead-date">${escapeHtml(formatHeadToHeadDateLabel(match))}</div>
+              <div class="club-history-datebox">
+                <div class="match-headtohead-date club-history-date">${escapeHtml(formatHeadToHeadDateLabel(match))}</div>
+                <div class="club-history-date-meta">${escapeHtml(tournamentLabel)}</div>
+              </div>
               <div class="match-headtohead-main">
                 <div class="match-headtohead-team match-headtohead-team-home">
                   <span class="match-headtohead-name">${escapeHtml(match.home_team)}</span>
